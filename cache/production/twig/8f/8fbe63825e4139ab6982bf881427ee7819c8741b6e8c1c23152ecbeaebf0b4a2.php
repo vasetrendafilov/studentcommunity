b053a09d015c7fc0c6b428691e3b1b630b035dd7
vasetrendafilov <vase_trendafilov@hotmail.com>
2020-02-02 23:01:09 +0100

    init

---
+++ cache/production/twig/8f/8fbe63825e4139ab6982bf881427ee7819c8741b6e8c1c23152ecbeaebf0b4a2.php
@@ -0,0 +1,120 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* acp_ext_actions.html */
+class __TwigTemplate_d6c4ccf927b7d8de507813baff345da9991f2ee309366ded21310538543cd875 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(twig_get_attribute($this->env, $this->source, ($context["enabled"] ?? null), "actions", [], "any", false, false, false, 1));
+        foreach ($context['_seq'] as $context["_key"] => $context["action"]) {
+            // line 2
+            echo "\t<a href=\"";
+            echo twig_get_attribute($this->env, $this->source, $context["action"], "U_ACTION", [], "any", false, false, false, 2);
+            echo "\"";
+            if (twig_get_attribute($this->env, $this->source, $context["action"], "L_ACTION_EXPLAIN", [], "any", false, false, false, 2)) {
+                echo " title=\"";
+                echo twig_get_attribute($this->env, $this->source, $context["action"], "L_ACTION_EXPLAIN", [], "any", false, false, false, 2);
+                echo "\"";
+            }
+            if (twig_get_attribute($this->env, $this->source, $context["action"], "COLOR", [], "any", false, false, false, 2)) {
+                echo " style=\"color: ";
+                echo twig_get_attribute($this->env, $this->source, $context["action"], "COLOR", [], "any", false, false, false, 2);
+                echo ";\"";
+            }
+            echo " data-ajax=\"";
+            echo twig_get_attribute($this->env, $this->source, $context["action"], "ACTION_AJAX", [], "any", false, false, false, 2);
+            echo "\" data-refresh=\"true\">";
+            echo twig_get_attribute($this->env, $this->source, $context["action"], "L_ACTION", [], "any", false, false, false, 2);
+            echo "</a>";
+            if ( !twig_get_attribute($this->env, $this->source, $context["action"], "S_LAST_ROW", [], "any", false, false, false, 2)) {
+                echo "&nbsp;|&nbsp;";
+            }
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['action'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 4
+        echo "
+";
+        // line 5
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(twig_get_attribute($this->env, $this->source, ($context["disabled"] ?? null), "actions", [], "any", false, false, false, 5));
+        foreach ($context['_seq'] as $context["_key"] => $context["action"]) {
+            // line 6
+            echo "\t<a href=\"";
+            echo twig_get_attribute($this->env, $this->source, $context["action"], "U_ACTION", [], "any", false, false, false, 6);
+            echo "\"";
+            if (twig_get_attribute($this->env, $this->source, $context["action"], "L_ACTION_EXPLAIN", [], "any", false, false, false, 6)) {
+                echo " title=\"";
+                echo twig_get_attribute($this->env, $this->source, $context["action"], "L_ACTION_EXPLAIN", [], "any", false, false, false, 6);
+                echo "\"";
+            }
+            if (twig_get_attribute($this->env, $this->source, $context["action"], "COLOR", [], "any", false, false, false, 6)) {
+                echo " style=\"color: ";
+                echo twig_get_attribute($this->env, $this->source, $context["action"], "COLOR", [], "any", false, false, false, 6);
+                echo ";\"";
+            }
+            echo " data-ajax=\"";
+            echo twig_get_attribute($this->env, $this->source, $context["action"], "ACTION_AJAX", [], "any", false, false, false, 6);
+            echo "\" data-refresh=\"true\">";
+            echo twig_get_attribute($this->env, $this->source, $context["action"], "L_ACTION", [], "any", false, false, false, 6);
+            echo "</a>";
+            if ( !twig_get_attribute($this->env, $this->source, $context["action"], "S_LAST_ROW", [], "any", false, false, false, 6)) {
+                echo "&nbsp;|&nbsp;";
+            }
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['action'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "acp_ext_actions.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  74 => 6,  70 => 5,  67 => 4,  41 => 2,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "acp_ext_actions.html", "");
+    }
+}
---
+++ cache/production/twig/e1/e16aced4c8fbe82a7f8e3f44f322da80764e185abba7cd4cf771356f2508a62f.php
@@ -0,0 +1,293 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* index_body.html */
+class __TwigTemplate_10900fa6ac08da7e5605675d31df1bcae7d2f4b96b624588c54de0b12c2b6b23 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        $location = "overall_header.html";
+        $namespace = false;
+        if (strpos($location, '@') === 0) {
+            $namespace = substr($location, 1, strpos($location, '/') - 1);
+            $previous_look_up_order = $this->env->getNamespaceLookUpOrder();
+            $this->env->setNamespaceLookUpOrder(array($namespace, '__main__'));
+        }
+        $this->loadTemplate("overall_header.html", "index_body.html", 1)->display($context);
+        if ($namespace) {
+            $this->env->setNamespaceLookUpOrder($previous_look_up_order);
+        }
+        // line 2
+        echo "
+<p class=\"";
+        // line 3
+        echo ($context["S_CONTENT_FLOW_END"] ?? null);
+        echo " responsive-center time";
+        if (($context["S_USER_LOGGED_IN"] ?? null)) {
+            echo " rightside";
+        }
+        echo "\">";
+        if (($context["S_USER_LOGGED_IN"] ?? null)) {
+            echo ($context["LAST_VISIT_DATE"] ?? null);
+        } else {
+            echo ($context["CURRENT_TIME"] ?? null);
+        }
+        echo "</p>
+";
+        // line 4
+        if (($context["S_USER_LOGGED_IN"] ?? null)) {
+            echo "<p class=\"responsive-center time\">";
+            echo ($context["CURRENT_TIME"] ?? null);
+            echo "</p>";
+        }
+        // line 5
+        echo "
+";
+        // line 6
+        // line 7
+        if (($context["U_MARK_FORUMS"] ?? null)) {
+            // line 8
+            echo "\t<div class=\"action-bar compact\">
+\t\t<a href=\"";
+            // line 9
+            echo ($context["U_MARK_FORUMS"] ?? null);
+            echo "\" class=\"mark-read rightside\" accesskey=\"m\" data-ajax=\"mark_forums_read\">";
+            echo $this->extensions['phpbb\template\twig\extension']->lang("MARK_FORUMS_READ");
+            echo "</a>
+\t</div>
+";
+        }
+        // line 12
+        // line 13
+        echo "
+";
+        // line 14
+        $location = "forumlist_body.html";
+        $namespace = false;
+        if (strpos($location, '@') === 0) {
+            $namespace = substr($location, 1, strpos($location, '/') - 1);
+            $previous_look_up_order = $this->env->getNamespaceLookUpOrder();
+            $this->env->setNamespaceLookUpOrder(array($namespace, '__main__'));
+        }
+        $this->loadTemplate("forumlist_body.html", "index_body.html", 14)->display($context);
+        if ($namespace) {
+            $this->env->setNamespaceLookUpOrder($previous_look_up_order);
+        }
+        // line 15
+        echo "
+";
+        // line 16
+        // line 17
+        echo "
+";
+        // line 18
+        // line 19
+        echo "
+";
+        // line 20
+        if (($context["S_DISPLAY_ONLINE_LIST"] ?? null)) {
+            // line 21
+            echo "\t<div class=\"stat-block online-list\">
+\t\t";
+            // line 22
+            if (($context["U_VIEWONLINE"] ?? null)) {
+                echo "<h3><a href=\"";
+                echo ($context["U_VIEWONLINE"] ?? null);
+                echo "\">";
+                echo $this->extensions['phpbb\template\twig\extension']->lang("WHO_IS_ONLINE");
+                echo "</a></h3>";
+            } else {
+                echo "<h3>";
+                echo $this->extensions['phpbb\template\twig\extension']->lang("WHO_IS_ONLINE");
+                echo "</h3>";
+            }
+            // line 23
+            echo "\t\t<p>
+\t\t\t";
+            // line 24
+            // line 25
+            echo "\t\t\t";
+            echo ($context["TOTAL_USERS_ONLINE"] ?? null);
+            echo " (";
+            echo $this->extensions['phpbb\template\twig\extension']->lang("ONLINE_EXPLAIN");
+            echo ")<br />";
+            echo ($context["RECORD_USERS"] ?? null);
+            echo "<br />
+\t\t\t";
+            // line 26
+            if (($context["U_VIEWONLINE"] ?? null)) {
+                // line 27
+                echo "\t\t\t\t<br />";
+                echo ($context["LOGGED_IN_USER_LIST"] ?? null);
+                echo "
+\t\t\t\t";
+                // line 28
+                if (($context["LEGEND"] ?? null)) {
+                    echo "<br /><em>";
+                    echo $this->extensions['phpbb\template\twig\extension']->lang("LEGEND");
+                    echo $this->extensions['phpbb\template\twig\extension']->lang("COLON");
+                    echo " ";
+                    echo ($context["LEGEND"] ?? null);
+                    echo "</em>";
+                }
+                // line 29
+                echo "\t\t\t";
+            }
+            // line 30
+            echo "\t\t\t";
+            // line 31
+            echo "\t\t</p>
+\t</div>
+";
+        }
+        // line 34
+        echo "
+";
+        // line 35
+        // line 36
+        echo "
+";
+        // line 37
+        if (($context["S_DISPLAY_BIRTHDAY_LIST"] ?? null)) {
+            // line 38
+            echo "\t<div class=\"stat-block birthday-list\">
+\t\t<h3>";
+            // line 39
+            echo $this->extensions['phpbb\template\twig\extension']->lang("BIRTHDAYS");
+            echo "</h3>
+\t\t<p>
+\t\t\t";
+            // line 41
+            // line 42
+            echo "\t\t\t";
+            if (twig_length_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["loops"] ?? null), "birthdays", [], "any", false, false, false, 42))) {
+                echo $this->extensions['phpbb\template\twig\extension']->lang("CONGRATULATIONS");
+                echo $this->extensions['phpbb\template\twig\extension']->lang("COLON");
+                echo " <strong>";
+                $context['_parent'] = $context;
+                $context['_seq'] = twig_ensure_traversable(twig_get_attribute($this->env, $this->source, ($context["loops"] ?? null), "birthdays", [], "any", false, false, false, 42));
+                foreach ($context['_seq'] as $context["_key"] => $context["birthdays"]) {
+                    echo twig_get_attribute($this->env, $this->source, $context["birthdays"], "USERNAME", [], "any", false, false, false, 42);
+                    if ((twig_get_attribute($this->env, $this->source, $context["birthdays"], "AGE", [], "any", false, false, false, 42) !== "")) {
+                        echo " (";
+                        echo twig_get_attribute($this->env, $this->source, $context["birthdays"], "AGE", [], "any", false, false, false, 42);
+                        echo ")";
+                    }
+                    if ( !twig_get_attribute($this->env, $this->source, $context["birthdays"], "S_LAST_ROW", [], "any", false, false, false, 42)) {
+                        echo ", ";
+                    }
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['birthdays'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                echo "</strong>";
+            } else {
+                echo $this->extensions['phpbb\template\twig\extension']->lang("NO_BIRTHDAYS");
+            }
+            // line 43
+            echo "\t\t\t";
+            // line 44
+            echo "\t\t</p>
+\t</div>
+";
+        }
+        // line 47
+        echo "
+";
+        // line 48
+        if (($context["NEWEST_USER"] ?? null)) {
+            // line 49
+            echo "\t<div class=\"stat-block statistics\">
+\t\t<h3>";
+            // line 50
+            echo $this->extensions['phpbb\template\twig\extension']->lang("STATISTICS");
+            echo "</h3>
+\t\t<p>
+\t\t\t";
+            // line 52
+            // line 53
+            echo "\t\t\t";
+            echo ($context["TOTAL_POSTS"] ?? null);
+            echo " &bull; ";
+            echo ($context["TOTAL_TOPICS"] ?? null);
+            echo " &bull; ";
+            echo ($context["TOTAL_USERS"] ?? null);
+            echo " &bull; ";
+            echo ($context["NEWEST_USER"] ?? null);
+            echo "
+\t\t\t";
+            // line 54
+            // line 55
+            echo "\t\t</p>
+\t</div>
+";
+        }
+        // line 58
+        echo "
+";
+        // line 59
+        // line 60
+        echo "
+";
+        // line 61
+        $location = "overall_footer.html";
+        $namespace = false;
+        if (strpos($location, '@') === 0) {
+            $namespace = substr($location, 1, strpos($location, '/') - 1);
+            $previous_look_up_order = $this->env->getNamespaceLookUpOrder();
+            $this->env->setNamespaceLookUpOrder(array($namespace, '__main__'));
+        }
+        $this->loadTemplate("overall_footer.html", "index_body.html", 61)->display($context);
+        if ($namespace) {
+            $this->env->setNamespaceLookUpOrder($previous_look_up_order);
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "index_body.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  261 => 61,  258 => 60,  257 => 59,  254 => 58,  249 => 55,  248 => 54,  237 => 53,  236 => 52,  231 => 50,  228 => 49,  226 => 48,  223 => 47,  218 => 44,  216 => 43,  190 => 42,  189 => 41,  184 => 39,  181 => 38,  179 => 37,  176 => 36,  175 => 35,  172 => 34,  167 => 31,  165 => 30,  162 => 29,  153 => 28,  148 => 27,  146 => 26,  137 => 25,  136 => 24,  133 => 23,  121 => 22,  118 => 21,  116 => 20,  113 => 19,  112 => 18,  109 => 17,  108 => 16,  105 => 15,  93 => 14,  90 => 13,  89 => 12,  81 => 9,  78 => 8,  76 => 7,  75 => 6,  72 => 5,  66 => 4,  52 => 3,  49 => 2,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "index_body.html", "");
+    }
+}
